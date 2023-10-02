17cb749e781742c59b134d84bc3cc8ebe8982f9f
Martin Coll <mcoll@dc.uba.ar>
2023-10-02 19:13:51 -0300

    dihypergraph from stdin

--- xgi_yaml.py
+++ xgi_yaml.py
@@ -1,17 +1,35 @@
 import yaml
+import sys
 import xgi
+import matplotlib.pyplot as plt
 
 
-def to_xgi(graph, doc):
-    name = "main"
-    match doc:
-        case yaml.ScalarNode(value=scalar):
-            graph.add_edge([[scalar], []])
-        case yaml.MappingNode(value=[*docs_pairs]):
-            for k, v in docs_pairs:
-                graph.add_edge([[k.value], [v.value]])
-                to_xgi(graph, k), to_xgi(graph, v)
-        case yaml.SequenceNode(value=[*docs]):
-            for k, v in zip(docs[:-1], docs[1:]):
-                graph.add_edge([[k.value], [v.value]])
-                to_xgi(graph, k), to_xgi(graph, v)
+def read_dihypergraph(node):
+	""""""
+	res = xgi.DiHypergraph()
+	match node:
+		case yaml.ScalarNode(value=scalar):
+			res.add_edge([[scalar], [scalar]])
+		case yaml.MappingNode(value=mapping):
+			all_keys = [read_dihypergraph(k) for k, v in mapping]
+			for k in all_keys:
+				res.add_edge([all_keys, k])
+			for k, v in mapping:
+				k, v = read_dihypergraph(k), read_dihypergraph(v)
+				res.add_edge([k, v])
+		case yaml.SequenceNode(value=sequence):
+			for l, r in zip(sequence[:-1], sequence[1:]):
+				l, r = read_dihypergraph(l), read_dihypergraph(r)
+				res.add_edge([l, r])
+	return res
+def main():
+	""""""
+	res = xgi.DiHypergraph()
+	for doc in yaml.compose_all(sys.stdin, yaml.SafeLoader):
+		res.add_edges_from(read_dihypergraph(doc).edges.dimembers())
+		#res.add_edge([[res], read_dihypergraph(doc)])
+	xgi.draw_dihypergraph(res, node_labels=True, node_size=50)
+	plt.show()
+
+if __name__ == "__main__":
+	main()
